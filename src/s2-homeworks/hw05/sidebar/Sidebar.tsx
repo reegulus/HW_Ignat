a6dd8d6c44a340843e704d86914ce723ccfbcb23
Egorych <mikhalevichegor@gmail.com>
2023-03-12 18:25:54 +0300

    Sidebar.tsx

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -12,6 +12,7 @@ type PropsType = {
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
+    const onActiveLink = (props:{isActive: boolean}) =>props.isActive ? s.active : ''
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={onActiveLink} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={onActiveLink} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +48,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={onActiveLink} // делает студент
                     >
                         Junior Plus
                     </NavLink>
